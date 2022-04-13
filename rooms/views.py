c8f7026b721a1c68093d1c48a1deaa02c8432e54
Ashish Kumar <ashishk8181@gmail.com>
2022-04-13 22:34:07 +0530

    Homepage View
    Configured url.py, added pagination to home

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from math import ceil
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from . import models
 
-# Create your views here.
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+
+    try:
+        rooms = paginator.page(page)
+    except PageNotAnInteger:
+        rooms = paginator.page(1)
+    except EmptyPage:
+        return redirect("/")
+
+    return render(request, "rooms/all_rooms.html", {"rooms": rooms})

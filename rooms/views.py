2b7931a80f333d212d83e150a377946190054bc8
Ashish Kumar <ashishk8181@gmail.com>
2022-05-04 18:50:56 +0530

    Added Search Fields

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
+from urllib import request
 from xml.parsers.expat import model
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django_countries import countries
 from . import models
 
 
@@ -17,3 +20,48 @@ class room_detail(DetailView):
     """Detail View Definition"""
 
     model = models.Room
+
+
+def search(request):
+
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    room_type = int(request.GET.get("room_type", 0))
+    country = request.GET.get("country", "IN")
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+    beds = int(request.GET.get("beds", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "baths": baths,
+        "beds": beds,
+        "instant": instant,
+        "super_host": super_host,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    options = {
+        "room_types": room_types,
+        "countries": countries,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **options})

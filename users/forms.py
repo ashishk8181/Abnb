b6a81ee7feda13ad848ac91132b82804ef86e750
Ashish Kumar <ashishk8181@gmail.com>
2022-05-10 17:29:37 +0530

    Sign up added

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,5 @@
+from dataclasses import field, fields
+from pyexpat import model
 from . import models
 from django import forms
 
@@ -19,3 +21,29 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Incorrect Password"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data["password"]
+        password1 = self.cleaned_data["password1"]
+
+        if password != password1:
+            raise forms.ValidationError("Passwords don't Match")
+        else:
+            return password
+
+    def save(self):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()

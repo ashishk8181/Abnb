26ccf6b06bfa7f9f2728cc1fb2c7e82464828339
Ashish Kumar <ashishk8181@gmail.com>
2022-06-27 00:23:37 +0530

    styled home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "65vh": "65vh",
+      },
+      width: {
+        "9/12": "87.5%"
+      }
+      // borderRadius: {
+      //   "xl": "1.5 rem"
+      // }
+
+    },
   },
   plugins: [],
 }
